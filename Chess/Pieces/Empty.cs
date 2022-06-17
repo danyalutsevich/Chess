85d3877b6fbe3ba41c75ea8afa8d0ab48b1b698b
danyalutsevich <luchevich31@gmail.com>
2022-06-17 10:32:27 +0300

    minor changes

--- Chess/Pieces/Empty.cs
+++ Chess/Pieces/Empty.cs
@@ -11,6 +11,7 @@ namespace Chess.Pieces
         public Empty(int x, int y, string? team) : base(x, y, team)
         {
         }
+        
 
         public override void ShowAvailableMoves()
         {
